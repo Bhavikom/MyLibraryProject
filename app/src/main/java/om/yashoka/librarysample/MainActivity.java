9368c12d32875a705c1d40e428e13ddd037f96a4
Bhavikom <bhavikmakvanarock@gmail.com>
2018-06-09 12:57:18 +0530

    Initial commit on 9th june

---
+++ app/src/main/java/om/yashoka/librarysample/MainActivity.java
@@ -0,0 +1,31 @@
+package om.yashoka.librarysample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import librarysample.yashoka.om.findingdistancelibs.LatLonDistanceCalculator;
+import librarysample.yashoka.om.findingdistancelibs.Point;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Point buenosAiresObeliscoPoint =
+                new Point((float) -34.6037389, (float) -58.3815704);
+
+        Point nycStatueOfLibertyPoint =
+                new Point((float) 40.6892494, (float) -74.0445004);
+
+        float distanceBetweenPoints = LatLonDistanceCalculator.calculateDistance(
+                buenosAiresObeliscoPoint,
+                nycStatueOfLibertyPoint);
+
+        TextView kilometersLabel = (TextView) findViewById(R.id.kilometers_label);
+
+        kilometersLabel.setText("Between the Obelisco and the Statue of Liberty there are: \n" + distanceBetweenPoints + "KM");
+    }
+}
